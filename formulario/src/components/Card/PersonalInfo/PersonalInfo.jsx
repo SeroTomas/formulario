bacf8905553f6c4b5b851f747e9e1c809a51bdbe
SeroTomas <serocontacto@gmail.com>
2023-05-13 16:55:41 -0300

    cambios en css global

--- formulario/src/components/Card/PersonalInfo/PersonalInfo.jsx
+++ formulario/src/components/Card/PersonalInfo/PersonalInfo.jsx
@@ -8,20 +8,19 @@ function PersonalInfo() {
 
     return (
         <>
-            <div className={style.personalContainer}>
-                <div className={style.infoContainer}>
-                    <h2>Personal info</h2>
-                    <div className={style.text}>
-                        <p>Please provide your name, email</p>
-                        <p>address, and phone number</p>
-                    </div>
+            <div className="infoContainer">
+                <h2>Personal info</h2>
+                <div className="text">
+                    <p>Please provide your name, email</p>
+                    <p>address, and phone number</p>
                 </div>
-                <form className={style.form}>
-                    <PersonalInput label={"Name"} name={"name"} type={"text"}/>
-                    <PersonalInput label={"Email Address"} name={"email"} type={"email"}/>
-                    <PersonalInput label={"Phone Number"} name={"phone"} type={"phone"}/>
-                </form>
             </div>
+            <form className={style.form}>
+                <PersonalInput label={"Name"} name={"name"} type={"text"} />
+                <PersonalInput label={"Email Address"} name={"email"} type={"email"} />
+                <PersonalInput label={"Phone Number"} name={"phone"} type={"phone"} />
+            </form>
+
         </>
 
     )
--- formulario/src/components/Card/SelectYourPlan/SelectYourPlan.jsx
+++ formulario/src/components/Card/SelectYourPlan/SelectYourPlan.jsx
@@ -1,7 +1,7 @@
 //styles
 import style from './selectYourPlan.module.scss';
 //components and hooks
-import React from 'react'
+import React, { useState } from 'react'
 import PlanSelector from './PlanSelector/PlanSelector'
 import Switch from '@mui/material/Switch';
 import arcadeIcon from '../../../../public/icon-arcade.svg';
@@ -9,26 +9,53 @@ import advancedIcon from '../../../../public/icon-advanced.svg';
 import proIcon from '../../../../public/icon-pro.svg';
 
 function SelectYourPlan() {
-    return (
 
-        <div>
-            <div>
-                <h2 className='h2Title'>Select your plan</h2>
-                <p className='pText'>You have the option of monthly or </p>
-                <p className='pText'>yearly billing</p>
+    const [plan, setPlan] = useState(null);
+    const [toggle, setToggle] = useState(true);
+    const prices = {
+        monthly: {
+            arcade: 9,
+            advanced: 12,
+            pro: 15
+        },
+        yearly: {
+            arcade: 90,
+            advanced: 120,
+            pro: 150
+        }
+    };
+
+    const label = { inputProps: { 'aria-label': 'Switch demo' } };
+
+    const handleChange = () => {
+        toggle ? setPlan(prices.monthly) : setPlan(prices.yearly);
+    };
+
+    const handleToggle = (e) => {
+        const value = e.target.value;
+        console.log(value)
+    };
+
+    return (
+        <>
+            <div className='infoContainer'>
+                <h2 >Select your plan</h2>
+                <div className='text'>
+                    <p>You have the option of monthly or </p>
+                    <p>yearly billing</p>
+                </div>
             </div>
             <div>
-                <PlanSelector />
-                <PlanSelector />
-                <PlanSelector />
+                <PlanSelector icon={arcadeIcon} title={"Arcade"}/>
+                <PlanSelector icon={advancedIcon} title={"Advanced"}/>
+                <PlanSelector icon={proIcon} title={"Pro"}/>
             </div>
             <div>
                 <p>Monthly</p>
-                <Switch/>
+                <Switch onChange={handleToggle}/>
                 <p>Yeraly</p>
             </div>
-        </div>
-
+        </>
     )
 }
 
